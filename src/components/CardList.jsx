7d74f81074d259513c1debf99e1a3ec1299270d4
Nano <mrosso.026@gmail.com>
2023-07-17 12:57:06 -0300

    🚀 [Performance]:  Uses API data on components

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,14 +1,37 @@
 import HeroCard from "./HeroCard";
-import classes from './CardList.module.css';
+import classes from "./CardList.module.css";
+import { useEffect, useState } from "react";
 
 function CardList() {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+      const jsonData = await response.json();
+      setCards(jsonData.results);
+    } catch (error) {
+      console.log("Error al obtener los datos:", error);
+    }
+  };
   return (
     <ul className={classes.ul}>
-      <HeroCard heroName="Spider-Man" image="/spiderman.jpg" />
-      <HeroCard heroName="Wolverine" image="/wolverine.jpg"/>
-      <HeroCard heroName="IronMan" image="/ironman.jpg"/>
-      <HeroCard heroName="Deadpool" image="/deadpool.jpg"/>
-      <HeroCard heroName="MoonKnight" image="/moonknight.jpg"/>
+      {cards.map((card) => {
+        const index = cards.indexOf(card);
+        return (
+          <HeroCard
+            key={index}
+            heroName={card.name}
+            image={
+              `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+            }
+          />
+        );
+      })}
     </ul>
   );
 }
