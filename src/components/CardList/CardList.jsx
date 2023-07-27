8da5138077ec87c7ce129df185634fa197438905
Nano <mrosso.026@gmail.com>
2023-07-27 15:16:02 -0300

    🐛 [Bug]: Fixes litte performing issue

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -3,25 +3,6 @@ import classes from "./CardList.module.css";
 import { useEffect, useState } from "react";
 
 function CardList({ isHeaderActive, objects, searchText }) {
-  console.log("1");
-  const [favList, setFavList] = useState(
-    JSON.parse(localStorage.getItem("favList")) || []
-  );
-  useEffect(() => {
-    const handleLocalStorageChange = (event) => {
-      console.log("event.key", event.key);
-      if (event.key === "favList") {
-        setFavList(JSON.parse(localStorage.getItem("favList")) || []);
-      }
-    };
-
-    window.addEventListener("storage", handleLocalStorageChange);
-
-    return () => {
-      window.removeEventListener("storage", handleLocalStorageChange);
-    };
-  }, []);
-
   const filteredComics = objects.filter(
     (hero) =>
       hero.name.toLowerCase().includes(searchText.toLowerCase()) ||
